2454fb2fd3e65fc378de0af9cb47f8bcbb9e609a
Dina Talal Muhanna <muhannadinatalal@gmail.com>
2026-04-06 15:50:42 +0200

    Update queries.sql

--- 01-Retail-Sales-Analysis/queries.sql
+++ 01-Retail-Sales-Analysis/queries.sql
@@ -1,36 +1,42 @@
-SELECT COUNT(*) AS NumberOfRows
-FROM Sales
-------------------------------
-SELECT TOP 5*
-FROM Sales
-------------------------------
-SELECT Product_Category , SUM(Total_Amount) AS Total_Amount
+-- ====================================
+-- Project: Retail Sales Analysis
+-- Tool: SQL Server (SSMS)
+-- Description: Analyzing retail sales 
+-- data to uncover business insights
+-- ====================================
+
+-- Q1: Total sales per Product Category
+-- Concept: GROUP BY + SUM
+SELECT Product_Category, SUM(Total_Amount) AS Total_Sales
 FROM Sales
 GROUP BY Product_Category
-ORDER BY SUM(Total_Amount) DESC
----------------------------------
+ORDER BY Total_Sales DESC;
+
+-- Q2: Top 5 days by total sales
+-- Concept: TOP + ORDER BY
 SELECT TOP 5 Date, SUM(Total_Amount) AS Total_Amount
 FROM Sales
 GROUP BY Date
-ORDER BY SUM(Total_Amount) DESC
------------------------------------------------
-SELECT Gender , SUM(Total_Amount) AS Total_Amount
-FROM Sales 
+ORDER BY SUM(Total_Amount) DESC;
+
+-- Q3: Which Gender spends more?
+-- Concept: GROUP BY + SUM
+SELECT Gender, SUM(Total_Amount) AS Total_Amount
+FROM Sales
 GROUP BY Gender
-ORDER BY SUM(Total_Amount) DESC
----------------------------------------------
-SELECT Product_Category , AVG(Total_Amount) AS [Avg Of Total Amount]
+ORDER BY SUM(Total_Amount) DESC;
+
+-- Q4: Average order value per Category
+-- Concept: AVG + GROUP BY
+SELECT Product_Category, AVG(Total_Amount) AS Avg_Total_Amount
 FROM Sales
 GROUP BY Product_Category
-ORDER BY AVG(Total_Amount) DESC
---------------------------------------------
-SELECT TOP 1 FORMAT(Date,'MMM') AS Month, SUM(Total_Amount) AS Total_Amount
+ORDER BY AVG(Total_Amount) DESC;
+
+-- Q5: Which month has the highest sales?
+-- Concept: FORMAT + DATE Functions
+SELECT TOP 1 FORMAT(Date, 'MMM') AS Month, 
+SUM(Total_Amount) AS Total_Amount
 FROM Sales
-GROUP BY FORMAT(Date,'MMM')
-ORDER BY SUM(Total_Amount) DESC
----------------------------------------------
-
-
-
-
-
+GROUP BY FORMAT(Date, 'MMM')
+ORDER BY SUM(Total_Amount) DESC;
